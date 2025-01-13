304d5a1f4da8f9ff5b1f418b95bb085fa56c4880
robinbiju16 <robinbbiju@gmail.com>
2025-01-13 12:51:13 +0530

    dartexamedited

--- bin/dart_exam.dart
+++ bin/dart_exam.dart
@@ -1,4 +1,82 @@
+import 'package:dart_exam/dart_exam.dart' as dart_exam;
+
 void main()
 {
-    
+
+List<int> num = [12, 45, 7, 89, 23];
+print("largest is: $num");
+
+
+//SECTION A
+
+//2. print even number upto 20
+
+
+ 
+  // print("print even numbers upto 20");
+  
+  // for (int i = 1; i <= 20; i++)
+  //  {
+  //   if (i % 2 == 0)
+  //    {
+  //     print(i);
+  //   }
+  // }
+
+//3. Sum of number in the list
+
+// List<int> num = [10,20,30,40,50];
+// int sum = 0;
+// for (int number in num) 
+// {
+//     sum += number;
+//   }
+
+// print("Sum is :  $sum");
+
+
+//5. function
+
+int largest(List<int> num)
+ {
+  if (num.isEmpty)
+  {
+    print('list must be not empty');
+  }
+  int largest = num[0];
+  for (int number in num) 
+  {
+    if (number > largest) 
+    {
+      largest = number;
+    }
+  }
+  return largest;
 }
+
+
+//6. patter for loop
+
+
+  
+  // for (int i = 1; i <= 4; i++) 
+  // {
+  //   String num = '';
+  //   for (int j = 1; j <= i; j++)
+  //    {
+  //     num += i.toString();
+  //   }
+  //   print(num);
+  // }
+
+
+
+//  Map<String, dynamic> person = 
+// {
+//     "name": "John","age": 30,"isStudent": false
+//   };
+//      print(person);
+
+}
+
+
